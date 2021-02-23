9db39ce06373d55a58e9f0b042b1cf5499550a11
佐藤悠真 <archyusa@icloud.com>
2021-02-23 00:55:02 +0000

    Added timestamp, line of code and filepath to print statements

---
+++ Sources/SwiftPrint/Setup.swift
@@ -0,0 +1,22 @@
+//
+// The SwiftPrint project.
+// Created by optionaldev on 23/02/2021.
+// Copyright © 2021 optionaldev. All rights reserved.
+// 
+
+import class Foundation.DateFormatter
+
+public extension SwiftPrint {
+    final class Setup {
+        private init() {}
+        
+        internal static let timestampDateFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm:ss.SSS"
+            return formatter
+        }()
+        
+        public static var isEnabled = true
+        public static var printNilMessages: Bool = false
+    }
+}
--- Sources/SwiftPrint/SwiftPrint.swift
+++ Sources/SwiftPrint/SwiftPrint.swift
@@ -1,3 +1,43 @@
-struct SwiftPrint {
-    var text = "Hello, World!"
+//
+// The SwiftPrint project.
+// Created by optionaldev on 22/02/2021.
+// Copyright © 2021 optionaldev. All rights reserved.
+//
+
+import struct Foundation.Date
+
+public enum SwiftPrint {
+    
+    // MARK: - Public
+    
+    public static func generateOutput(message messageOrNil: Any?,
+                                        object: AnyObject?,
+                                        filePath: String,
+                                        lineOfCode: UInt) -> String? {
+        guard messageOrNil != nil || (messageOrNil == nil && Setup.printNilMessages) else
+        {
+            return nil
+        }
+        
+        let message = messageOrNil != nil ? "\(messageOrNil!)" : "nil"
+        
+        return "\(currentDateAsString) | \(fileNameFromPath(filePath)):\(lineOfCodeString(lineOfCode)) | \(message)"
+    }
+    
+    // MARK: - Internal
+    
+    // Prefixes number of lines of code with 2 spaces for numbers 0-10 and with 1 space for numbers 10-99
+    internal static func lineOfCodeString(_ lineOfCode: UInt) -> String {
+        return lineOfCode < 10 ? "  \(lineOfCode)" : (lineOfCode < 100 ? " \(lineOfCode)" : "\(lineOfCode)")
+    }
+    
+    // Having "/" in the name is not valid, so the only question remainins:
+    // Are there situations in which you have swift files with multiple periods?
+    internal static func fileNameFromPath(_ filePath: String) -> String {
+        return String(filePath.split(separator: "/").last?.split(separator: ".").first ?? "")
+    }
+    
+    internal static var currentDateAsString: String {
+        Setup.timestampDateFormatter.string(from: Date())
+    }
 }
